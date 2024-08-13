ae868e9d8c9b4512432b7ac517e658af730d9040
michaleq44 <michalokul@gmail.com>
2024-08-13 11:24:23 +0200

    struct ops and funcs v1

--- include/physics_vectors.hpp
+++ include/physics_vectors.hpp
@@ -1,12 +1,16 @@
+#include <cmath>
 typedef struct vec2 {
-    int x, y;
+    int64_t x, y;
 };
 typedef struct vec3 {
-    int x, y, z;
+    int64_t x, y, z;
 };
 typedef struct vec4 {
-    int x, y, z, w;
+    vec3 v;
+    int64_t w;
 };
+
+// operation operators {
 
 bool operator==(const vec2& lhs, const vec2& rhs) {
     return lhs.x == rhs.x && lhs.y == rhs.y;
@@ -15,5 +19,33 @@ bool operator==(const vec3& lhs, const vec3& rhs) {
     return lhs.x == rhs.x && lhs.y == rhs.y && lhs.z == rhs.z;
 }
 bool operator==(const vec4& lhs, const vec4& rhs) {
-    return lhs.x == rhs.x && lhs.y == rhs.y && lhs.z == rhs.z && lhs.w == rhs.w;
+    return lhs.v == rhs.v && lhs.w == rhs.w;
 }
+vec3 operator+(const vec3& lhs, const vec3& rhs) {
+    return {lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z};
+}
+vec3 operator-(const vec3& lhs, const vec3& rhs) {
+    return {lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z};;
+}
+vec3 operator*(const vec3& lhs, const int& rhs) {
+    return {lhs.x * rhs, lhs.y * rhs, lhs.z * rhs};
+}
+vec3 operator*(const int& lhs, const vec3& rhs) {
+    return {rhs.x * lhs, rhs.y * lhs, rhs.z * lhs};
+}
+vec3 operator*(const vec3& lhs, const vec3& rhs) {
+    return {lhs.x * rhs.x, lhs.y * rhs.y, lhs.z * rhs.z};
+}
+
+// }
+
+// operation functions {
+
+int _vec3norm(const vec3& x) {
+    return sqrt(x.x*x.x + x.y*x.y + x.z*x.z);
+}
+vec3 _vec3cross(vec3& x, vec3& y) {
+    return {x.y*y.z - x.z*y.y, x.z*y.x - x.x*y.z, x.x*y.y - x.y*y.x};
+}
+
+// }
